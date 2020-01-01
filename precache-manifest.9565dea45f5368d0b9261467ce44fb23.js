0e8ca41da9a67c48dac9b719ab23fca5650c7699
Soheil Sadeghbayan <soheilsadeghbayan@yahoo.com>
2020-01-01 13:19:20 +0330

    Updates

---
+++ precache-manifest.9565dea45f5368d0b9261467ce44fb23.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "073a93b0ec109a9854be2a45616e1545",
+    "url": "/Cloud-storage-subscription/index.html"
+  },
+  {
+    "revision": "f250709af10fd437297d",
+    "url": "/Cloud-storage-subscription/static/css/2.7f669fb7.chunk.css"
+  },
+  {
+    "revision": "b6cce3193d9e91d8e18a",
+    "url": "/Cloud-storage-subscription/static/css/main.9c473277.chunk.css"
+  },
+  {
+    "revision": "f250709af10fd437297d",
+    "url": "/Cloud-storage-subscription/static/js/2.7623dafb.chunk.js"
+  },
+  {
+    "revision": "87984cad984dae3222b2a1d0861003a4",
+    "url": "/Cloud-storage-subscription/static/js/2.7623dafb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b6cce3193d9e91d8e18a",
+    "url": "/Cloud-storage-subscription/static/js/main.7788a6f3.chunk.js"
+  },
+  {
+    "revision": "2ea9190d2750141df853",
+    "url": "/Cloud-storage-subscription/static/js/runtime-main.e362e4f7.js"
+  }
+]);
